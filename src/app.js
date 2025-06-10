3a39a7d2a18c264814203ca50918e9fa089141a2
Andrei Mikheikin <mikhejyshka@gmail.com>
2025-06-10 14:37:30 +0300

    app use fix

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 // Основной роутер
 app.use('/api/users', userRoutes);
-app.use('/api/adminDashboard/', poolOfUserRoutes);
+app.use('/api/adminDashboard', poolOfUserRoutes);
 
 app.get('/health', (_, res) => res.send('OK'));
 
