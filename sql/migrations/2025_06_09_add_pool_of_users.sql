320bb35bd1b63327ba03e85c396ba06ef55241e5
Andrei Mikheikin <mikhejyshka@gmail.com>
2025-06-10 11:15:43 +0300

    migration alter fix

--- sql/migrations/2025_06_09_add_pool_of_users.sql
+++ sql/migrations/2025_06_09_add_pool_of_users.sql
@@ -11,18 +11,28 @@ CREATE TABLE IF NOT EXISTS pool_of_users (
 
 -- 2. Обновление таблицы recipients (admin_id)
 ALTER TABLE recipients
-  ADD COLUMN IF NOT EXISTS admin_id INT NOT NULL,
-  ADD CONSTRAINT fk_recipients_admin FOREIGN KEY (admin_id) REFERENCES users(id) ON DELETE CASCADE;
+  ADD COLUMN IF NOT EXISTS admin_id INT NOT NULL;
+
+ALTER TABLE recipients
+  ADD CONSTRAINT IF NOT EXISTS fk_recipients_admin FOREIGN KEY (admin_id) REFERENCES users(id) ON DELETE CASCADE;
 
 -- 3. Обновление таблицы templates (admin_id)
 ALTER TABLE templates
-  ADD COLUMN IF NOT EXISTS admin_id INT NOT NULL,
-  ADD CONSTRAINT fk_templates_admin FOREIGN KEY (admin_id) REFERENCES users(id) ON DELETE CASCADE;
+  ADD COLUMN IF NOT EXISTS admin_id INT NOT NULL;
+
+ALTER TABLE templates
+  ADD CONSTRAINT IF NOT EXISTS fk_templates_admin FOREIGN KEY (admin_id) REFERENCES users(id) ON DELETE CASCADE;
 
 -- 4. Обновление таблицы send_history:
 -- добавил ссылку на pool_of_users и admin
 ALTER TABLE send_history
-  ADD COLUMN IF NOT EXISTS pool_user_id INT,
-  ADD COLUMN IF NOT EXISTS admin_id INT,
-  ADD CONSTRAINT fk_send_history_user FOREIGN KEY (pool_user_id) REFERENCES pool_of_users(id) ON DELETE SET NULL,
-  ADD CONSTRAINT fk_send_history_admin FOREIGN KEY (admin_id) REFERENCES users(id) ON DELETE SET NULL;
+  ADD COLUMN IF NOT EXISTS pool_user_id INT;
+
+ALTER TABLE send_history
+  ADD COLUMN IF NOT EXISTS admin_id INT;
+
+ALTER TABLE send_history
+  ADD CONSTRAINT IF NOT EXISTS fk_send_history_user FOREIGN KEY (pool_user_id) REFERENCES pool_of_users(id) ON DELETE SET NULL;
+
+ALTER TABLE send_history
+  ADD CONSTRAINT IF NOT EXISTS fk_send_history_admin FOREIGN KEY (admin_id) REFERENCES users(id) ON DELETE SET NULL;
