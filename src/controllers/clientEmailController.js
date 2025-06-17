bb37ea3f5569c7bb3580cd7d5f060da21273749b
Andrei Mikheikin <mikhejyshka@gmail.com>
2025-06-17 09:33:06 +0300

    fix: text in sendClientEmail

--- src/controllers/clientEmailController.js
+++ src/controllers/clientEmailController.js
@@ -2,16 +2,16 @@ import EmailSendLog from '../models/emailSendLog.js';
 import { sendHtmlEmail } from '../services/emailService.js'; // функция отправки email
 
 export const sendClientEmail = async (req, res) => {
-  const { recipient_email, subject, html } = req.body;
+  const { recipient_email, subject, html, text } = req.body;
   const { id: user_id, admin_id, email: sender_email } = req.user;
 
-  if (!recipient_email || !subject || !html) {
+  if (!recipient_email || !subject || (!html && !text)) {
     return res.status(400).json({ error: 'Неверные входные данные' });
   }
 
   try {
     await sendHtmlEmail({
-      from: sender_email, // или кастомный адрес отправителя, если есть
+      from: sender_email,
       to: recipient_email,
       subject,
       html: html || `<pre>${text}</pre>`
@@ -21,7 +21,8 @@ export const sendClientEmail = async (req, res) => {
 
     res.json({ message: 'Email успешно отправлен' });
   } catch (err) {
-    console.error('Ошибка при отправке письма:', err);
-    res.status(500).json({ error: 'Не удалось отправить письмо' });
+    console.error('Ошибка при отправке письма:', err.message);
+    console.error(err.stack);
+    res.status(500).json({ error: 'Не удалось отправить письмо', details: err.message });
   }
 };
