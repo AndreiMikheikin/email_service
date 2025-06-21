22a046f0118bc9d3aae5488b1d37c09d9a02c4d8
Andrei Mikheikin <mikhejyshka@gmail.com>
2025-06-21 21:27:54 +0300

    fix: renderToString

--- apps/admin-spa/src/entry-server.jsx
+++ apps/admin-spa/src/entry-server.jsx
@@ -1,24 +1,13 @@
 // entry-server.jsx
 import React from 'react';
-import { renderToPipeableStream } from 'react-dom/server';
+import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
 import AppRoutes from './app';
 
-export async function render(url) {
-  let didError = false;
-  const stream = renderToPipeableStream(
+export function render(url) {
+  return renderToString(
     <StaticRouter location={url} basename="/admin-spa">
       <AppRoutes />
-    </StaticRouter>,
-    {
-      onShellReady() {
-        res.status(didError ? 500 : 200);
-        stream.pipe(res);
-      },
-      onError(error) {
-        didError = true;
-        console.error(error);
-      }
-    }
+    </StaticRouter>
   );
 }
